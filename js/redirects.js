c3718f256787b011f0d52604222cd35e6f854b7d
KayoticCarnige <32397453+kckarnige@users.noreply.github.com>
2022-12-23 22:59:29 -0500

    No one gets left behind >:(

---
+++ js/redirects.js
@@ -0,0 +1,7 @@
+if (window.location.pathname.split('/')[1] == "BoundFight") {
+    document.location.href = "https://kckarnige.is-a.dev/hoat";
+} else if (window.location.pathname.split('/')[1] == ("OrbitNX" || "SaturnNX")) {
+    document.location.href = "https://github.com/OrbitNX/OrbitNX";
+} else if (window.location.pathname.split('/')[1] == ("custom-server-logos" || "DiscordCSL")) {
+    document.location.href = "https://github.com/DiscordCSL/DiscordCSL";
+}
