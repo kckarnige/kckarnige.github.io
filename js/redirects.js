7dcae37c42a110605c32e8027debb9be5f41cd7f
KayoticCarnige <32397453+kckarnige@users.noreply.github.com>
2022-12-24 08:49:31 -0500

    Update redirects.js

--- js/redirects.js
+++ js/redirects.js
@@ -1,7 +1,11 @@
 if (window.location.pathname.split('/')[1] == "BoundFight") {
     document.location.href = "https://kckarnige.is-a.dev/hoat";
-} else if (window.location.pathname.split('/')[1] == "OrbitNX") {
+}
+
+if (window.location.pathname.split('/')[1] == "OrbitNX") {
     document.location.href = "https://github.com/OrbitNX/OrbitNX";
-} else if (window.location.pathname.split('/')[1] == "custom-server-logos"||"DiscordCSL") {
+}
+
+if (window.location.pathname.split('/')[1] == "custom-server-logos"||"DiscordCSL") {
     document.location.href = "https://github.com/DiscordCSL/DiscordCSL";
 }
