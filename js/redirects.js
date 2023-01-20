562fc10f6db99cabed8baa2b5896b654688e5042
KayoticCarnige <32397453+kckarnige@users.noreply.github.com>
2023-01-20 10:20:48 -0500

    Update redirects.js

--- js/redirects.js
+++ js/redirects.js
@@ -6,4 +6,6 @@ if (window.location.pathname.split('/')[1] == "BoundFight") {
     document.location.href = "https://github.com/DiscordCSL/DiscordCSL";
 } else if (window.location.pathname.split('/')[1] == "custom-server-logos") {
     document.location.href = "https://github.com/DiscordCSL/DiscordCSL";
+} else if (window.location.pathname.split('/')[1] == "armcord") {
+    document.location.href = "https://armcord.xyz/";
 }
