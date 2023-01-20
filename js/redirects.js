426371c2d22fbaa5add12aa8764fc34e55c59778
KayoticCarnige <32397453+kckarnige@users.noreply.github.com>
2023-01-20 10:32:20 -0500

    Update redirects.js

--- js/redirects.js
+++ js/redirects.js
@@ -1,11 +1,19 @@
 if (window.location.pathname.split('/')[1] == "BoundFight") {
     document.location.href = "https://kckarnige.is-a.dev/hoat";
+    document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "OrbitNX") {
     document.location.href = "https://github.com/OrbitNX/OrbitNX";
+    document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "DiscordCSL") {
     document.location.href = "https://github.com/DiscordCSL/DiscordCSL";
+    document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "custom-server-logos") {
     document.location.href = "https://github.com/DiscordCSL/DiscordCSL";
+    document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "armcord") {
     document.location.href = "https://armcord.xyz/";
+    document.getElementById("main").remove();
+} else if (window.location.pathname.split('/')[1] == "sans") {
+    document.location.href = "https://kckarnige.is-a.dev/BadTimeRevamped";
+    document.getElementById("main").remove();
 }
