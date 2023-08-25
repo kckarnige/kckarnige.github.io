ac6da4b4b832d718a9f23a3c5d131a7d51a61953
KayoticCarnige <32397453+kckarnige@users.noreply.github.com>
2023-08-25 19:21:23 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -39,7 +39,15 @@ document.getElementById("topbar").innerHTML = `
                 </svg></a>
         </li>
         <li>
-            <a href="https://modrinth.com/user/kckarnige" target="_blank">Modrinth<svg viewBox="0 0 24 24"
+            <a href="https://modrinth.com/user/NX46xZR9" target="_blank">Modrinth<svg viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+                    <path fill="none" d="M0 0h24v24H0V0z" />
+                    <path
+                        d="M19 19H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" />
+                </svg></a>
+        </li>
+        <li>
+            <a href="https://www.planetminecraft.com/member/kayoticcarnige/" target="_blank">Planet Minecraft<svg viewBox="0 0 24 24"
                     xmlns="http://www.w3.org/2000/svg" width="24" height="24">
                     <path fill="none" d="M0 0h24v24H0V0z" />
                     <path
