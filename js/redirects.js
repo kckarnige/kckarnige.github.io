302a57442fc1a4936d8761cb1ba864d7ca8ace8d
KayoticCarnige <kayos156official@gmail.com>
2023-10-03 10:00:31 -0400

    Update redirects.js

--- js/redirects.js
+++ js/redirects.js
@@ -16,4 +16,7 @@ if (window.location.pathname.split('/')[1] == "BoundFight") {
 } else if (window.location.pathname.split('/')[1] == "sans") {
     document.location.href = "https://kckarnige.is-a.dev/BadTimeRevamped";
     document.getElementById("main").remove();
+} else if (window.location.pathname.split('/')[1] == "sex") {
+    document.location.href = "https://kckarnige.is-a.dev";
+    document.getElementById("main").remove();
 }
