12c4383f4e377433e423ff0cc22850b51f20ecbe
KayoticCarnige <32397453+kckarnige@users.noreply.github.com>
2023-10-23 12:51:37 -0400

    More clear when you're being redirected

--- js/redirects.js
+++ js/redirects.js
@@ -1,25 +1,33 @@
 if (window.location.pathname.split('/')[1] == "BoundFight") {
     document.location.href = "https://kckarnige.is-a.dev/hoat";
+    document.title = "Redirecting..";
     document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "OrbitNX") {
     document.location.href = "https://github.com/OrbitNX/OrbitNX";
+    document.title = "Redirecting..";
     document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "DiscordCSL") {
     document.location.href = "https://github.com/DiscordCSL/DiscordCSL";
+    document.title = "Redirecting..";
     document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "custom-server-logos") {
     document.location.href = "https://github.com/DiscordCSL/DiscordCSL";
+    document.title = "Redirecting..";
     document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "armcord") {
     document.location.href = "https://armcord.xyz/";
+    document.title = "Redirecting..";
     document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "sans") {
     document.location.href = "https://kckarnige.is-a.dev/BadTimeRevamped";
+    document.title = "Redirecting..";
     document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "sex") {
     document.location.href = "https://kckarnige.is-a.dev";
+    document.title = "Redirecting..";
     document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "OculusKillSwitch") {
     document.location.href = "https://github.com/kckarnige/OculusKillSwitch";
+    document.title = "Redirecting..";
     document.getElementById("main").remove();
 }
