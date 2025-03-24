9ddac25ba9c1bc162de5ec394c2a46639f2bf149
KCK <kayos156official@gmail.com>
2025-03-23 22:12:03 -0400

    Future proof
    And some fixes

--- script/redirects.js
+++ script/redirects.js
@@ -1,48 +1,65 @@
 if (window.location.pathname.split('/')[1] == "BoundFight") {
+    // Garbage
     document.location.href = "https://kckarnige.github.io/hoat";
     document.title = "Redirecting..";
     document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "OrbitNX") {
+    // Old project
     document.location.href = "https://github.com/OrbitNX/OrbitNX#readme";
     document.title = "Redirecting..";
     document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "DiscordCSL") {
+    // Old project
     document.location.href = "https://github.com/DiscordCSL/DiscordCSL#readme";
     document.title = "Redirecting..";
     document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "custom-server-logos") {
+    // Old project
     document.location.href = "https://github.com/DiscordCSL/DiscordCSL#readme";
     document.title = "Redirecting..";
     document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "armcord") {
-    document.location.href = "https://armcord.xyz/";
+    // LegCord
+    document.location.href = "https://github.com/Legcord/Legcord";
+    document.title = "Redirecting..";
+    document.getElementById("main").remove();
+} else if (window.location.pathname.split('/')[1] == "legcord") {
+    // Legcord
+    document.location.href = "https://github.com/Legcord/Legcord";
     document.title = "Redirecting..";
     document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "sans") {
-    document.location.href = "https://kckarnige.github.io/BadTimeRevamped#readme";
+    // Bad Time Sim: Reimagined
+    document.location.href = "https://kckarnige.github.io/BadTimeRevamped";
     document.title = "Redirecting..";
     document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "redir") {
+    // Shush
     document.location.href = "https://kckarnige.github.io";
     document.title = "Redirecting..";
     document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "OculusKillSwitch") {
+    // Oculus Kill Switch
     document.location.href = "https://github.com/kckarnige/OculusKillSwitch#readme";
     document.title = "Redirecting..";
     document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "tscm") {
+    // The Subjectively Correct Modpack
     document.location.href = "https://modrinth.com/modpack/tscm";
     document.title = "Redirecting..";
     document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "iatj") {
+    // It's About The Journey
     document.location.href = "https://iatj-modpack.github.io";
     document.title = "Redirecting..";
     document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "mc-sns") {
+    // Minecraft: Sticks n' Stones
     document.location.href = "https://mc-sns.github.io";
     document.title = "Redirecting..";
     document.getElementById("main").remove();
 } else if (window.location.pathname.split('/')[1] == "atlyss-pack") {
+    // ATLYSS: TheKCKModpack
     document.location.href = "https://thunderstore.io/c/atlyss/p/kckarnige/TheKCKModpack/";
     document.title = "Redirecting..";
     document.getElementById("main").remove();
