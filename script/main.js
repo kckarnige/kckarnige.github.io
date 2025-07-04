88f365cfd55586a68d21c8362d1cf42c1e4b3198
KiCKTheBucket <kayos156official@gmail.com>
2025-07-04 13:20:07 -0400

    Reorder

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-var topProjectNums = [22, 2, 19, 0];
+var topProjectNums = [19, 2, 22, 0];
 let portfolioListNum;
 function disableScroll() {
   document.documentElement.classList.add("scrollByebye")
